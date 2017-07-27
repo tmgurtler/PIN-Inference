f43e7419f8ec4521f3f0c0d72b512840db01e069
clm5067 <clm5067@rit.edu>
2017-07-27 11:24:27 -0400

    completed structure of node class

---
+++ node.py
@@ -0,0 +1,69 @@
+##
+# Node class for the implementation of the PIN inference tree
+##
+
+from distance import Distance
+
+
+class Node:
+    def __init__(self, digit, p_digit):
+        # TODO determine distance
+        # d = Distance(p_digit, digit)
+        self.digit = digit
+        self.probability = 0.0
+        self.distance = 0
+        self.children = []
+
+    ##
+    # @returns the digit for this node
+    ##
+    def get_digit(self):
+        return self.digit
+
+    ##
+    # @returns the probability for this node
+    ##
+    def get_probability(self):
+        return self.probability
+
+    ##
+    # @returns the distance for this node
+    ##
+    def get_distance(self):
+        return self.distance
+
+    ##
+    # @returns the list of children for this node
+    ##
+    def get_children(self):
+        return self.children
+
+    ##
+    # This function calculates the probability of this node
+    # in regards to this node's parent probability
+    ##
+    def calc_probability(self, parent, model, timing):
+        self.probability = parent.probability()\
+                           * model.probability(timing, self.distance)
+
+    ##
+    # This function adds the given child to the list of children
+    # for this node
+    ##
+    def add_child(self, child):
+        self.children.append(child)
+
+    # TODO delete or modify this function
+    def display(self, level):
+        print(self.digit)
+        for child in self.children:
+            child.display()
+
+    # TODO delete or modify this function
+    def size(self):
+        count = 1
+        for child in self.children:
+            count += child.size()
+        return count
+
+# End of file
